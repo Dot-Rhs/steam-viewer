0fe631c71e2e2b981f9ca48ec41b58d3bde57914
Dot-Rhs <rhysh1986@gmail.com>
2024-12-28 18:07:27 +0000

    prod change

--- vite.config.ts
+++ vite.config.ts
@@ -17,11 +17,18 @@ export default ({ mode, command }: { mode: string; command: unknown }) => {
       // },
       define: {
         "process.env": {
-          VITE_LOCAL_SERVER_API_BASE_DOMAIN: "http://localhost:5000",
-          VITE_LOCAL_GAMES_API_BASE_DOMAIN: "http://localhost:5001",
-          VITE_LOCAL_PLAYERS_API_BASE_DOMAIN: "http://localhost:5002",
+          VITE_LOCAL_SERVER_API_BASE_DOMAIN: "http://127.0.0.1:5000",
+          VITE_LOCAL_GAMES_API_BASE_DOMAIN: "http://127.0.0.1:5001",
+          VITE_LOCAL_PLAYERS_API_BASE_DOMAIN: "http://127.0.0.1:5002",
         },
       },
+      server: {
+        port: (Number(process.env.PORT) as number) || 3001,
+      },
+    });
+  } else {
+    return defineConfig({
+      plugins: [react()],
       server: {
         port: (Number(process.env.PORT) as number) || 3001,
       },
